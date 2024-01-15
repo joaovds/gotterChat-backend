a5a08095d130a93b78017daab33bf8a3b9cc9fb8
João Victor da Silva <jv782063@gmail.com>
2024-01-15 09:51:53 -0300

    configure websocket fiber

--- infra/fiber/routes/setupRoutes.go
+++ infra/fiber/routes/setupRoutes.go
@@ -7,9 +7,5 @@ import (
 func SetupRoutes(app *fiber.App) {
 	apiV1 := app.Group("/api/v1")
 
-  apiV1.Get("/ping", func(c *fiber.Ctx) error {
-    return c.JSON(fiber.Map{
-      "message": "pong",
-    })
-  })
+  handleWebsocketRoutes(apiV1)
 }
