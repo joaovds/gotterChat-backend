147cac6f62486c8f5f09f0f34bd38c7b15e4cf24
Miguel Brito <miguel.brito@levva.io>
2024-01-19 21:45:25 -0300

    feat: getByNickName route addition

--- infra/webserver/routes/setupRoutes.go
+++ infra/webserver/routes/setupRoutes.go
@@ -20,8 +20,9 @@ func SetupRoutes(mainMux *chi.Mux, userRepo *repository.UserRepository) {
 }
 
 func handleUserRoutes(mux *chi.Mux, userRepo *repository.UserRepository) {
+	mux.Mount("/user", mux)
 
-	mux.Post("/createUser", func(writer http.ResponseWriter, request *http.Request) {
+	mux.Post("/create", func(writer http.ResponseWriter, request *http.Request) {
 		var newUser domain.User
 		if err := json.NewDecoder(request.Body).Decode(&newUser); err != nil {
 			http.Error(writer, "Error decodifying JSON", http.StatusBadRequest)
@@ -48,6 +49,36 @@ func handleUserRoutes(mux *chi.Mux, userRepo *repository.UserRepository) {
 			return
 		}
 
+		writer.WriteHeader(http.StatusCreated)
 		writer.Write([]byte("User created successfully."))
+	})
+
+	mux.Get("/getByNickName", func(writer http.ResponseWriter, request *http.Request) {
+		nickname := request.URL.Query().Get("nickname")
+
+		if nickname == "" {
+			http.Error(writer, "Nickname parameter is required.", http.StatusBadRequest)
+			return
+		}
+
+		dbUser, err := userRepo.GetUserByNickName(nickname)
+
+		if err != nil && err.Error() != "User not found" {
+			http.Error(writer, "There was an error getting the user by the given nickname.", http.StatusInternalServerError)
+			return
+		} else if err != nil && err.Error() == "User not found" {
+			http.Error(writer, "User not found", http.StatusNotFound)
+			return
+		}
+
+		jsonResponse, err := json.Marshal(dbUser)
+		if err != nil {
+			http.Error(writer, "Error serializing user object to JSON.", http.StatusInternalServerError)
+			return
+		}
+
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusOK)
+		writer.Write(jsonResponse)
 	})
 }
