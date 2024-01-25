163f8643719371c0fe15f31e48214f62c6603443
João Victor da Silva <jv782063@gmail.com>
2024-01-25 15:04:31 -0300

    fix: converts bson to json correctly, using id to _id when in the database scope

--- internal/data/dtos/user.go
+++ internal/data/dtos/user.go
@@ -1,7 +1,7 @@
 package dtos
 
 type CreateUserInputDTO struct {
-  ID string `json:"id" bson:"id"`
+  ID string `json:"id" bson:"_id"`
   Nickname string `json:"nickname" bson:"nickname"`
   Password string `json:"password" bson:"password"`
 }
